02c697649db77454dc817c4020e985b60ec368ef
Void1Tech <voidtechcorpo@gmail.com>
2024-07-16 21:52:10 -0300

    Delete project directory

--- project/commentary.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('#comment').addEventListener('input', commentarySize);
-function commentarySize() {
-    const commentary = document.getElementById('comment').value;
-    if (commentary.length > 600) {
-        document.getElementById('comment').setCustomValidity("Excede el limite de los 600 caracteres");
-    } else {
-        document.getElementById('comment').setCustomValidity("");
-    }
-}
